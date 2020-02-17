2307adb75034059b08cfc513310cc26ce1a969fa
sidart069 <34807389+sidart069@users.noreply.github.com>
2020-02-17 10:52:49 +0530

    Add files via upload

---
+++ aa1.c
@@ -0,0 +1,100 @@
+#include<stdio.h>
+#include<string.h>
+
+char terminals[] = {'a','h','c','b','g','f','$'};
+char nonTerminals[] = {'S','B','C','D','E','F'};
+
+char table[6][7][4] = {{"aBDh","","","","","",""},
+{"","","cC","","","",""},
+{"","e","","bC","e","e",""},
+{"","EF","","","EF","EF",""},
+{"","e","","","g","e",""},
+{"","e","","","","f",""}};
+
+char stack[10], input[10], cs;
+int top;
+
+int getTerminal(char c) {
+int i;
+for(i=0; i<7; i++)
+if(terminals[i] == c)
+return i;
+return -1;
+}
+
+int getNonTerminal(char c) {
+int i;
+// printf("\nSearching for %c.\n",c);
+for(i=0; i<6; i++)
+if(nonTerminals[i] == c)
+return i;
+return -1;
+}
+
+int main() {
+int i, term, nonTerm;
+char temp[10];
+printf("Enter string:\t");
+scanf("%s",input);
+input[strlen(input)] = '$';
+
+cs=0;
+strcpy(stack,"$S");
+top = strlen(stack) - 1;
+printf("\nStack\tInput\tOutput\n");
+
+while(stack[top] != '$') {
+for(i=0; i<=top; i++)
+printf("%c", stack[i]);
+printf("\t");
+for(i=cs; i<strlen(input); i++)
+printf("%c", input[i]);
+printf("\t");
+if(input[cs] == stack[top]) {
+top--;
+cs++;
+printf("Match");
+}
+else {
+term = getTerminal(input[cs]);
+nonTerm = getNonTerminal(stack[top]);
+
+top--;
+if(term == -1 || nonTerm == -1) {
+printf("\nInvalid string.\n");
+return 0;
+}
+strcpy(temp, table[nonTerm][term]);
+if(!strcmp(temp, "")) {
+printf("\nInvalid string.\n");
+return 0;
+}
+else if(temp[0] != 'e') {
+for(i=strlen(temp)-1; i>=0; i--) {
+top++;
+stack[top] = temp[i];
+}
+}
+printf("%c->%s",nonTerminals[nonTerm],temp);
+}
+printf("\n");
+
+}
+printf("Valid string.\n");
+
+
+for(int i=0;i<6;i++){
+ for(int j=0;j<7;j++){
+  for(int k=0;k<4;k++){
+     printf("%c",table[i][j][k]);
+  }
+  printf(" ");
+ }
+  printf("\n");
+}
+
+
+
+
+return 0;
+}
---
+++ aa2.c
@@ -0,0 +1,117 @@
+/*2.   Predictive parsing table for
+     E->TX
+     X-> +TX | epsilon
+     T->FY
+     Y-> *FY | epsilon
+     F-> (E) | i
+*/
+
+
+#include<stdio.h>
+#include<string.h>
+
+char terminals[] = {'i','+','*','(',')','$'};
+char nonTerminals[] = {'E','X','T','Y','F'};
+
+char table[5][6][4] = {{"TX","","","TX","",""},
+{"","+TX","","","e","e"},
+{"FY","","","FY","",""},
+{"","e","*FY","","e","e"},
+{"i","","","(E)","",""}};
+
+char stack[10], input[10], cs;
+int top;
+
+int getTerminal(char c) {
+int i;
+for(i=0; i<6; i++)
+if(terminals[i] == c)
+return i;
+return -1;
+}
+
+int getNonTerminal(char c) {
+int i;
+// printf("\nSearching for %c.\n",c);
+for(i=0; i<5; i++)
+if(nonTerminals[i] == c)
+return i;
+return -1;
+}
+
+int main() {
+int i, term, nonTerm;
+char temp[10];
+printf("Enter string:\t");
+scanf("%s",input);
+input[strlen(input)] = '$';
+
+cs=0;
+strcpy(stack,"$E");
+top = strlen(stack) - 1;
+printf("\nStack\tInput\tOutput\n");
+
+while(stack[top] != '$') {
+for(i=0; i<=top; i++)
+printf("%c", stack[i]);
+printf("\t");
+for(i=cs; i<strlen(input); i++)
+printf("%c", input[i]);
+printf("\t");
+if(input[cs] == stack[top]) {
+top--;
+cs++;
+printf("Match");
+}
+else {
+term = getTerminal(input[cs]);
+nonTerm = getNonTerminal(stack[top]);
+
+top--;
+if(term == -1 || nonTerm == -1) {
+printf("Invalid string.\n");
+if(term == -1)
+printf("Term\n");
+else
+printf("Non Term\n");
+return 0;
+}
+strcpy(temp, table[nonTerm][term]);
+if(!strcmp(temp, "")) {
+printf("Invalid string.\n");
+return 0;
+}
+else if(temp[0] != 'e') {
+for(i=strlen(temp)-1; i>=0; i--) {
+top++;
+stack[top] = temp[i];
+}
+}
+printf("%c->%s",nonTerminals[nonTerm],temp);
+}
+printf("\n");
+
+}
+printf("Valid string.\n");
+
+
+
+for(int i=0;i<5;i++){
+ for(int j=0;j<6;j++){
+  for(int k=0;k<4;k++){
+     printf("%c",table[i][j][k]);
+  }
+  printf(" ");
+ }
+  printf("\n");
+}
+
+
+
+
+
+
+
+
+return 0;
+}
